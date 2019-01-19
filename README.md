9217dda94da1caa2b7fed939d864d34d123af386
vimentor-com <43086913+vimentor-com@users.noreply.github.com>
2019-01-20 00:09:10 +0700

    Mã nguồn cơ sở cho lộ trình python backend
    Mã nguồn bao gồm các file đầu vào (file ảnh jpg), file code python
    file quản lý các gói phụ trợ (requirements.txt). Trong đó mã nguồn python
    thực hiện các việc sau:
    1. Đọc ảnh đầu vào
    2. Cắt ảnh đầu vào thành 3 ảnh nhỏ hơn
    3. Chuyển mầu tất cả các ảnh nhỏ sang đen trắng (grayscale)
    4. Gộp các ảnh nhỏ đã được chuyển màu thành một ảnh lớn
    5. Lưu ảnh vào cùng thư mục với ảnh đầu vào

---
+++ README.md
@@ -0,0 +1,4 @@
+# pythonbackendpathdemo
+Đây là github repo chứa mã nguồn cho lộ trình học python backend 
+Lộ trình này có thể tìm thấy trên trang web https://vimentor.com ở đường dẫn sau: https://vimentor.com/vi/learning-path/lo-trinh-hoc-python-cho-ky-su-backend?type=lesson
+Group facebôk để thảo luận các vấn đề liên quan tới lộ trình học: https://www.facebook.com/groups/711152479268593/
---
+++ house3d_worker_demo.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+
+def show_img(img, winname="TEST"):
+    from matplotlib import pyplot
+    pyplot.figure()
+    pyplot.imshow(img, cmap='gray')
+    pyplot.title(winname)
+    pyplot.show()
+
+
+def image_cut(img, num=10):
+    print("Hien thi anh goc")
+    show_img(img, "Anh dau vao")
+    small_imgs_dic = {}
+    high = img.shape[0]
+    smal_high = high // num
+
+    for i in range(num):
+        if i < num - 1:
+            simage = img[i * smal_high:(i + 1) * smal_high:, :, :]
+            small_imgs_dic[i] = convert_to_grayscale(simage)
+        else:
+            simage = img[i * smal_high:, :, ]
+            small_imgs_dic[i] = convert_to_grayscale(simage)
+        print("Hien thi anh da duoc cat va chuyen mau {}".format(i+1))
+        show_img(small_imgs_dic[i], "Anh nho {}".format(i+1))
+
+    return small_imgs_dic
+
+
+def convert_to_grayscale(image):
+    converted_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    return converted_img
+
+
+def image_merge(small_imags_dic):
+    images = []
+    for i in sorted(small_imags_dic.keys()):
+        images.append(small_imags_dic[i])
+    return np.concatenate(images, axis=0)
+
+
+if __name__ == '__main__':
+    img = cv2.imread("images/input_sample.jpg")
+    print('Cut anh goc thanh 3 anh nho hon')
+    cut_image = image_cut(img, num=3)
+    print('Ghep lai cac anh nho da duoc cat va chuyen mau')
+    merged_img = image_merge(cut_image)
+    print("Hien thi anh sau khi da chuyen mau")
+    show_img(merged_img, "Anh sau khi da duoc xu ly")
+    print("Luu anh da chuyen doi vao cung thu muc voi anh goc")
+    cv2.imwrite("images/output_sample.jpg", merged_img)
+    print("Hoan thanh viec chuyen mau cho anh")
